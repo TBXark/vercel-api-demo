a420156a47d0790936934ec85bd6efb1d1d5f84a
TBXark <tbxark@outlook.com>
2020-12-31 17:38:44 +0800

    update file structure

--- pages/api/typescript.ts
+++
@@ -1,6 +0,0 @@
-import { NowRequest, NowResponse } from '@vercel/node'
-
-export default function (req: NowRequest, res: NowResponse) {
-    const { name = 'NULL' } = req.query
-    res.send(`Typescript: Hello ${name}!`)
-}
