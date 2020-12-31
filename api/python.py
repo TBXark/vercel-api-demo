47418779591776525fbe1e12e7ee49f4d3173f7a
TBXark <tbxark@outlook.com>
2020-12-31 18:03:49 +0800

    update python demo

--- api/python.py
+++ api/python.py
@@ -8,7 +8,7 @@ class handler(BaseHTTPRequestHandler):
         self.end_headers()
         name = "NULL"
         try:
-            name = [x for i,x in enumerate(text.split("?")[1].split("&")) if x.split("=")[0] == "name"][0].split("=")[1]
+            name = [x for i,x in enumerate(self.path.split("?")[1].split("&")) if x.split("=")[0] == "name"][0].split("=")[1]
         except:
             pass
         self.wfile.write(("Python: Hello %s!" % name).encode())
